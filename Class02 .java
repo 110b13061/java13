cf3e9ba07865818ddf9ca3a007d6bed48e9dc7fe
110b13061 <113488308+110b13061@users.noreply.github.com>
2022-12-10 13:14:26 +0800

    Create Class02 .java

---
+++ Class02 .java
@@ -0,0 +1,15 @@
+package java13;
+
+public class Class02 {
+	public static void main(String args[]) {
+		int num=12,den=0;
+		int ans;
+		if(den==0) {
+			System.out.println("除數為0");
+		}
+		else {
+			ans=num/den;
+			System.out.println("ans="+ans);
+		}
+	}
+}
